4a9624979a0c877e172ba0ecd5359b02385c4cf5
Espumita <davidfalconnaranjo@gmail.com>
2019-11-14 20:08:20 +0000

    Add controller stuff

--- MyMusic/MyMusic.Api/Controllers/PlaylistsController.cs
+++ MyMusic/MyMusic.Api/Controllers/PlaylistsController.cs
@@ -3,12 +3,26 @@
 namespace MyMusic.Controllers {
 
     [Route("[controller]")]
-    public class PlaylistController: Controller {
+    public class PlaylistsController: Controller {
 
-        [HttpGet]
-        public void Get() {
-            
-            
+        [HttpGet("{playlistId}")]
+        public void Get(string playlistId) {
+            //Get the playlist
+        }
+        
+        [HttpPost]
+        public void Create() {
+            //Create the playlist
+        }
+        
+        [HttpPut("{playlistId}")]
+        public void Update(string playlistId) {
+            //ChangeName
+        }
+        
+        [HttpDelete("{playlistId}")]
+        public void Delete(string playlistId) {
+            //Delete the playlist                        
         }
     }
 }
