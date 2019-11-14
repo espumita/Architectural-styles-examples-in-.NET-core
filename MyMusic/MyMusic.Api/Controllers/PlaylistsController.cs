0e02c689ef13688319e99ccc16a383c93e7f7d61
Espumita <davidfalconnaranjo@gmail.com>
2019-11-14 19:22:54 +0000

    Init api

---
+++ MyMusic/MyMusic.Api/Controllers/PlaylistsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyMusic.Controllers {
+
+    public class PlaylistController: Controller {
+
+        [HttpGet]
+        public void Get() {
+            
+            
+        }
+    }
+}
