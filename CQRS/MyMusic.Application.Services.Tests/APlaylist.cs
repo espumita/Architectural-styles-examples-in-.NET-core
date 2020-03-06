a192cfd0db23b9851bc181b53848e1c4d002719f
Espumita <davidfalconnaranjo@gmail.com>
2020-03-06 15:07:08 +0000

    Delete unnecesary mock

--- CQRS/MyMusic.Application.Services.Tests/APlaylist.cs
+++ CQRS/MyMusic.Application.Services.Tests/APlaylist.cs
@@ -4,7 +4,6 @@ namespace MyMusic.Application.Services.Tests {
     public static class APlaylist {
         
         public static string Id = new Faker().Random.String2(64);
-        public static string AnotherId = new Faker().Random.String2(64);
         public static string Name = new Faker().Random.String2(64);
         public static string AnotherName = new Faker().Random.String2(64);
         public static string ImageUrl = new Faker().Random.String2(64);
