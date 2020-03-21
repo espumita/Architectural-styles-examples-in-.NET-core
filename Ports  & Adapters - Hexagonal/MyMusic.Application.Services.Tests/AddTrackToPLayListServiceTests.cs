e0b0584bde61749746a44df98bd9cf4366c6beaf
Espumita <davidfalconnaranjo@gmail.com>
2020-03-21 12:31:47 +0000

    Refactor: inline method VerifyErrorIs

--- Ports  & Adapters - Hexagonal/MyMusic.Application.Services.Tests/AddTrackToPLayListServiceTests.cs
+++ Ports  & Adapters - Hexagonal/MyMusic.Application.Services.Tests/AddTrackToPLayListServiceTests.cs
@@ -56,7 +56,7 @@ namespace MyMusic.Application.Services.Tests {
             var result = addTrackToPlayListService.Execute(aTrackId, aPlaylistId);
 
             result.IsLeft.Should().BeTrue();
-            VerifyErrorIs(ServiceError.CannotAddSameTrackTwice, result);
+            result.IfLeft(error => error.Should().Be(ServiceError.CannotAddSameTrackTwice));
             playListPersistence.DidNotReceive().Persist(Arg.Any<PlayList>());
             tracksNotifier.DidNotReceive().NotifyTrackHasBeenAddedToPlayList(Arg.Any<string>(), Arg.Any<string>());
         }
@@ -66,10 +66,6 @@ namespace MyMusic.Application.Services.Tests {
                 playlist.Id.Equals(aPlaylistId)
                 && playlist.TrackList.Single().Id.Equals(aTrackId)
             ));
-        }
-
-        private static void VerifyErrorIs(ServiceError serviceError, Either<ServiceError, ServiceResponse> result) {
-            result.IfLeft(error => error.Should().Be(serviceError));
         }
     }
 }
