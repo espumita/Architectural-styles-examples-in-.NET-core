942b98eb83b20248b8c5195dcb017c8a59ef4d17
Espumita <davidfalconnaranjo@gmail.com>
2020-03-21 12:54:06 +0000

    Refactor: inline VerifyAreEquivalent

--- Ports  & Adapters - Hexagonal/MyMusic.Application.Services.Tests/GetAllPlayListServiceTests.cs
+++ Ports  & Adapters - Hexagonal/MyMusic.Application.Services.Tests/GetAllPlayListServiceTests.cs
@@ -49,20 +49,12 @@ namespace MyMusic.Application.Services.Tests {
             var result = getAllPlayListService.Execute();
 
             result.IsRight.Should().BeTrue();
-            VerifyPLayListIsEquivalentTo(aPlayList, result);
+            result.IfRight(listOfPlayLists => VerifyAreEquivalent(listOfPlayLists, aPlayList));
         }
 
-        private static void VerifyPLayListIsEquivalentTo(PlayList aPlayList, Either<ServiceError, ListOfPlayLists> result) {
-            result.Match(
-                Right: listOfPlayLists => Validate(listOfPlayLists, aPlayList),
-                Left: error => null
-            );
-        }
-
-        private static PlayList Validate(ListOfPlayLists playListsList, PlayList expectedPlayList) {
+        private static void VerifyAreEquivalent(ListOfPlayLists playListsList, PlayList expectedPlayList) {
             var playList = playListsList.Elements.Single();
             playList.Should().BeEquivalentTo(expectedPlayList);
-            return expectedPlayList;
         }
     }
 }
